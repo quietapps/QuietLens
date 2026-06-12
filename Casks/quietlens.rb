ae8ad23fb81fe5043deb0e92cac990d89c7f1280
Parth Thummar <parththummar@gmail.com>
2026-06-11 20:18:40 -0400

    chore: pin quietlens cask sha256 for 1.0.7

--- Casks/quietlens.rb
+++ Casks/quietlens.rb
@@ -1,6 +1,6 @@
 cask "quietlens" do
   version "1.0.7"
-  sha256 :no_check  # set by scripts/release.sh output
+  sha256 "46e98edcaf2939278b76b43172149f38fc6c069d825644ec6025a0839fc021c3"
 
   url "https://github.com/quietapps/QuietLens/releases/download/#{version}/QuietLens-#{version}.zip",
       verified: "github.com/quietapps/QuietLens/"
